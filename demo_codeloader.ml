0ad529a3585d1149513b32e278f424cff25b637f
Daniel Richman <main@danielrichman.co.uk>
2015-06-10 23:07:47 +0000

    use assembler for demo_codeloader

--- demo_codeloader.ml
+++ demo_codeloader.ml
@@ -1,15 +1,12 @@
 open Core.Std
 
-(*    0x0000000000000000 <+0>: 48 ff c0  inc    %rax
- *    0x0000000000000003 <+3>: c3        retq        *)
-
-let _code = "\x48\xff\xc0\xc3"
-
-(*    0x0000000000000000 <+0>: 48 01 d8  add    %rbx,%rax
- *    0x0000000000000003 <+3>: 48 ff c8  dec    %rax
- *    0x0000000000000006 <+6>: c3        retq              *)
-
-let code = "\x48\x01\xd8\x48\xff\xc8\xc3"
+let code =
+  let open Assembler.Std in
+  assemble
+   [ I.ADD { I.source = A.Reg64 R.RBX; dest = A.Reg64 R.RAX }
+   ; I.DEC (A.Reg64 R.RAX)
+   ; I.RET
+   ]
 
 let closure : int -> int -> int =
   let module C = Codeloader in
